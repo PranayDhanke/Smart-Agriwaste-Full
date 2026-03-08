eb4bc9f4d37dfa7701c1e9446a6b9522cc1463d6
PranayDhanke <pranaydhanke33@gmail.com>
2026-03-08 11:08:43 +0530

    minor changes

--- frontend/src/components/page/waste/wasteforms/WasteForm.tsx
+++ frontend/src/components/page/waste/wasteforms/WasteForm.tsx
@@ -412,7 +412,7 @@ export default function ListWaste() {
                             label: "☀️ Dry",
                           },
                           {
-                            value: "semiwet",
+                            value: "semi_wet",
                             label: "🌤️ Semi-wet",
                           },
                           {
